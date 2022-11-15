c854020317a69038fbfdb2e16c6724678f865329
Danila Avetisian <avetisyandanila21@yandex.ru>
2022-11-16 00:27:20 +0400

    Добавил плавный скролл для якорей

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './index.html';
 import './sass/index.scss';
 import './modules/tabs';
+import './modules/smoothScroll';
---
+++ src/modules/smoothScroll.js
@@ -0,0 +1,47 @@
+import {forEach, set} from 'lodash';
+
+class SmoothScroll {
+  constructor(elem) {
+    this.block = elem;
+    this.init();
+  }
+
+  init() {
+    this.block.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      const href = this.block.getAttribute('href').substring(1);
+      const scrollTarget = document.getElementById(href);
+      const topOffset = document.querySelector('.header').offsetHeight;
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - topOffset;
+
+      window.scrollBy({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    })
+  }
+
+  static getAllBlocks() {
+    return Array.from(document.querySelectorAll('a[href^="#"]'));
+  }
+
+  static initAllBlocks() {
+    let Blocks = SmoothScroll.getAllBlocks();
+    forEach(Blocks, (block) => {
+      if (block.ins) return;
+      block.ins = new SmoothScroll(block);
+    });
+  }
+}
+
+// Инициализация;
+window.addEventListener('load', () => {
+  SmoothScroll.initAllBlocks();
+})
+
+// Экспорт;
+set(window, ['Block', 'SmoothScroll'], SmoothScroll);
+export default SmoothScroll;
+
