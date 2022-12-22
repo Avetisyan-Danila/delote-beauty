11797aaf3df4ee83fbb74121a5ca89bd4f235f70
Danila Avetisian <avetisyandanila21@yandex.ru>
2022-12-23 00:05:36 +0400

    Сделал адаптив главной для <576. Также поправил адаптив в других местах

--- src/modules/sideMenu.js
+++ src/modules/sideMenu.js
@@ -8,7 +8,13 @@ const headerBlackout = document.querySelector('.header__blackout');
 // Константы
 const sideMenuZIndex = '101';
 const sideMenuAnimationDuration = 400;
-const sideMenuPaddingLeft = 80;
+const sideMenuPaddingLeft = () => {
+  if (document.documentElement.clientWidth <= 576) {
+    return 40;
+  } else {
+    return 80;
+  }
+};
 const burgerWidth = 40;
 
 // Изначальное положение при загрузке
@@ -19,7 +25,9 @@ burger.style.top = burgerStartPositionTop;
 burger.style.left = burgerStartPositionLeft;
 burger.style.zIndex = sideMenuZIndex;
 
+// Обработчий клика по бургеру
 burger.addEventListener('click', () => {
+  // Запретить скролл на сайте и паддинг для скроллбаров, чтобы контент не прыгал
   document.querySelector('body').classList.add('lock');
   document.querySelector('body').style.paddingRight = `${getScrollBarWidth()}px`;
 
@@ -30,7 +38,7 @@ burger.addEventListener('click', () => {
 
   if (burger.classList.contains('burger--active')) {
     setTimeout(() => {
-      burger.style.left = `${sideMenu.getBoundingClientRect().left + sideMenuPaddingLeft}px`;
+      burger.style.left = `${sideMenu.getBoundingClientRect().left + sideMenuPaddingLeft()}px`;
       burger.style.pointerEvents = 'auto';
 
       headerBlackout.classList.add('header__blackout--active')
@@ -48,9 +56,10 @@ burger.addEventListener('click', () => {
   }
 })
 
+// Обработка резайза страницы
 window.addEventListener('resize', () => {
   if (burger.classList.contains('burger--active')) {
-    burger.style.left = `${sideMenu.getBoundingClientRect().left + sideMenuPaddingLeft}px`;
+    burger.style.left = `${sideMenu.getBoundingClientRect().left + sideMenuPaddingLeft()}px`;
     burgerStartPositionLeft = `${headerWrapper.getBoundingClientRect().right - burgerWidth}px`;
   } else {
     burgerStartPositionLeft = `${headerWrapper.getBoundingClientRect().right - burgerWidth}px`;
